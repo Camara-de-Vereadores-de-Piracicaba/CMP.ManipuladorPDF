5971c66e528ba328955b7ec860e9427dd672b319
Fabio Cardoso <fabio.piratininga@gmail.com>
2024-05-10 11:13:50 -0300

    Extração de Páginas.

--- Assinador/DocumentoPDF.cs
+++ Assinador/DocumentoPDF.cs
@@ -59,6 +59,31 @@ namespace CMP.ManipuladorPDF
             return pdfDocument.GetNumberOfPages();
         }
 
+        public static DocumentoPDF ExtrairPagina(this DocumentoPDF documento, int pagina=1)
+        {
+            using MemoryStream outputStream = new MemoryStream();
+            using PdfWriter pdfWriter = new PdfWriter(outputStream);
+            using PdfDocument pdfDocument = new PdfDocument(new PdfReader(new MemoryStream(documento.ByteArray)));
+            PdfDocument newPdfDocument = new PdfDocument(pdfWriter);
+            pdfDocument.CopyPagesTo(pagina, pagina, newPdfDocument);
+            pdfDocument.Close();
+            newPdfDocument.Close();
+            return new DocumentoPDF(outputStream.ToArray());
+        }
+
+        public static DocumentoPDF ExtrairPaginas(this DocumentoPDF documento, int inicio = 1, int fim = 1)
+        {
+            using MemoryStream outputStream = new MemoryStream();
+            using PdfWriter pdfWriter = new PdfWriter(outputStream);
+            using PdfDocument pdfDocument = new PdfDocument(new PdfReader(new MemoryStream(documento.ByteArray)));
+            PdfDocument newPdfDocument = new PdfDocument(pdfWriter);
+            pdfDocument.CopyPagesTo(inicio, fim, newPdfDocument);
+            pdfDocument.Close();
+            newPdfDocument.Close();
+            return new DocumentoPDF(outputStream.ToArray());
+        }
+
+
     }
 
 }
